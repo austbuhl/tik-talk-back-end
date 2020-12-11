ddc80978f5d87edf65beed7870632bad863d7895
AB <austinbuhler@gmail.com>
2020-12-10 21:26:13 -0500

    Messages creating

--- dbMessages.js
+++ dbMessages.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const tikTalkSchema = mongoose.Schema({
   message: String,
   name: String,
-  timestamp: String
+  timestamp: String,
+  sent: Boolean
 })
 
 export default mongoose.model('messageContent', tikTalkSchema)
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 // imports
 import express from 'express'
 import mongoose from 'mongoose'
+import Messages from './dbMessages.js'
+import Pusher from 'pusher'
 
 // app config
 const app = express()
 const port = process.env.PORT || 9000
+const pusher = new Pusher({
+  appId: '1121422',
+  key: '1e8a1765529bbbac589e',
+  secret: '95133b8c0ece2495fff2',
+  cluster: 'us2',
+  useTLS: true
+})
 
 // middleware
+app.use(express.json())
 
 // DB config
 const connectionUrl =
@@ -18,10 +28,29 @@ mongoose.connect(connectionUrl, {
   useUnifiedTopology: true
 })
 
-// ????
-
 // api routes
 app.get('/', (req, res) => res.status(200).send('hello world'))
+
+app.get('/api/v1/messages', (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  })
+})
+
+app.post('/api/v1/messages/', (req, res) => {
+  const dbMessage = req.body
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(data)
+    }
+  })
+})
 
 // listen
 app.listen(port, () => console.log(`Listening on localhost:${port}`))
